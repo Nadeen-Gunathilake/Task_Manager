a51efb6fb66c64f12dd18448ce9403a0ba93dff9
Nadeen-Gunathilake <nadeen.guna05@gmail.com>
2024-12-14 22:24:19 +0530

    Did some corrections

---
+++ frontend/src/components/NavBarLoggedInView.tsx
@@ -0,0 +1,35 @@
+import { Button, Navbar } from "react-bootstrap";
+import { User } from "../models/user";
+import * as NotesApi from "../network/notes_api";
+
+interface NavBarLoggedInViewProps{
+    user:User,
+    onLogoutSucessful:()=>void,
+}
+
+
+const NavBarLoggedInView = ({user,onLogoutSucessful}:NavBarLoggedInViewProps) => {
+
+    async function logout() {
+        try {
+            await NotesApi.logout();
+            onLogoutSucessful();
+        } catch (error) {
+            console.error(error);
+            alert(error);
+            
+        }
+        
+    }
+
+    return ( 
+        <>
+        <Navbar.Text className="me-2">
+            Signed in as:{user.username}
+        </Navbar.Text>
+        <Button onClick={logout}>Log out</Button>
+        </>
+     );
+}
+ 
+export default NavBarLoggedInView;
